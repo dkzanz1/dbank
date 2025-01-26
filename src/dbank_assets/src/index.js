a3d786a9bc1c74d0bf6ca9b4642e1af870b41e8f
paulzolik <paul.zolik@sky.com>
2025-01-26 13:38:41 +0000

    added diasable button

--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -1,11 +1,29 @@
 import { dbank } from "../../declarations/dbank";
 
 window.addEventListener("load", async function(){
-  const currentAmount = dbank.checkBalance();
-  document.getElementById("value").innerText = currentAmount;
-  console.log("finishing loading");
+  const currentAmount = await dbank.checkBalance();
+  document.getElementById("value").innerText = Math.round(currentAmount *100)/100;
+  // console.log("finishing loading");
 })
+document.querySelector("form").addEventListener ("submit", async function(e){
+// console.log("Submitted");
+e.preventDefault();
+const button =e.target.querySelector("#submit-btn");
 
+const inputAmount = parseFloat(document.getElementById("input-amount").value);
+const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+
+button.setAttribute("disabled", true);
+//line above disables button while sums input catches up
+await dbank.topUp(inputAmount);
+
+const currentAmount = await dbank.checkBalance();
+document.getElementById("value").innerText = Math.round(currentAmount *100)/100;
+
+document.getElementById("input-amount").value="";
+button.removeAttribute("disabled");
+
+});
 
 
 
